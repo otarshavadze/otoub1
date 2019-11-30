d4f6b37de5dd1c9b54efa6dce58ddf77352b4519
user2 <user2@gmail.com>
2019-11-30 21:33:58 +0400

    second big change

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <div>ccc</div>
 <div>aaa</div>
 <div>bbb</div>
-<div>ccc</div>
+<div>ccc1</div>
 <div>aaa</div>
 <div>bbb</div>
 <div>ccc</div>
@@ -190,7 +190,7 @@
 <div>aaa</div>
 <div>bbb</div>
 <div>ccc</div>
-<div>aaa</div>
+
 <div>bbb</div>
 <div>ccc</div>
 <div>aaa</div>
@@ -332,5 +332,5 @@
 <div>bbb</div>
 <div>ccc</div>
 <div>aaa</div>
-<div>bbb</div>
+<div>bbb2</div>
 <div>ccc</div>
